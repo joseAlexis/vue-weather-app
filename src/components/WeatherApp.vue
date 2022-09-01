8b0f4c7bf21fd8b08d2a59877bd62f03fad3cd0d
JoseB <jo.albol22@gmail.com>
2022-09-01 18:07:27 +0200

    Adding account

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -3,23 +3,21 @@
         <div class="weather-wrap">
             <div class="search-box">
                 <input type="text" placeholder="Search..." class="search-bar" v-model="query"
-                    v-on:keypress="fetchWeather" />
-            </div>
+                    v-on:keypress="fetchWeather" cy-data="searchRegion" />
 
-            <!-- <div class="weather-info" v-if="regions.length > 0">
-                <li v-for="(region, index) in regions" :key="index">
-                    <a href="#" @click="fetchWeather">{{ region.name }} - {{ region.country }}</a>
-                </li>
-            </div> -->
+                <button cy-data="userAccount" class="button">
+                    <img src="../assets/user-account.png" alt="User Account" class="img-login">
+                </button>
+            </div>
 
             <div class="weather-info" v-if="weather.main !== undefined">
                 <div class="location-box">
-                    <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-                    <div class="date">{{ todaysDate() }}</div>
+                    <div class="location" cy-data="region">{{ weather.name }}, {{ weather.sys.country }}</div>
+                    <div class="date" cy-data="date">{{ todaysDate() }}</div>
                 </div>
                 <div class="weather-box">
-                    <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-                    <div class="weather">{{ weather.weather[0].main }}</div>
+                    <div class="temp" cy-data="temp">{{ Math.round(weather.main.temp) }}°c</div>
+                    <div class="weather" cy-data="weather">{{ weather.weather[0].main }}</div>
                     <div class="icon">
                         <img :src="`${siteUrl}/img/wn/${weather.weather[0].icon}${'@2x.png'}`" />
                     </div>
@@ -45,22 +43,9 @@ export default {
         }
     },
     methods: {
-        // async fetchRegion(e) {
-        //     if (e.key === "Enter") {
-        //         let response = await axios.get(`${this.url_base}/geo/1.0/direct?q=${this.query}&units=metric&APPID=${this.api_key}`);
-        //         console.log(response.data)
-        //         this.setResults(response.data);
-        //     }
-        // },
-        // async fetchWeather2() {
-        //     let response = await axios.get(`${this.url_base}/data/3.0/onecall?lat=${this.lat}&lon=${this.long}&APPID=${this.api_key}`);
-        //     this.setResults(response.data);
-        // },
         async fetchWeather(e) {
-            console.log('API KEY' + process.env.VUE_APP_API_KEY)
             if (e.key === "Enter") {
-                let response = await axios.get(`${this.apiUrl}/data/2.5/weather?q=${this.query}&units=metric&APPID=${process.env.VUE_APP_API_KEY}`);
-                console.log(response.data)
+                let response = await axios.get(`${this.apiUrl}/data/2.5/weather?q=${this.query}&units=metric&APPID=${this.api_key}`);
                 this.setResults(response)
             }
         },
@@ -77,23 +62,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&display=swap");
+
 * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-family: "Montserrat";
 }
+
 .weather-container {
     background-image: url("../assets/background.jpg");
     background-size: cover;
     background-position: center;
     transition: 0.4s;
-    width: 375px;
+    width: 450px;
     margin: 0 auto;
     border-radius: 25px;
     margin-top: 50px;
     box-shadow: 0px 0px 30px #00000065;
 }
+
 .weather-wrap {
     height: 600px;
     padding: 25px;
@@ -102,6 +90,7 @@ export default {
             rgba(0, 0, 0, 0.15),
             rgba(0, 0, 0, 0.4));
 }
+
 .search-box .search-bar {
     display: block;
     width: 100%;
@@ -117,10 +106,19 @@ export default {
     border-radius: 10px;
     transition: 0.4s;
 }
+
 .search-box .search-bar:focus {
     box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
     background-color: rgba(255, 255, 255, 0.75);
 }
+
+.search-box .button {
+    float: right;
+    margin: 0;
+    border: 0;
+    background-color: transparent;
+}
+
 .location-box .location {
     color: #fff;
     font-size: 32px;
@@ -129,15 +127,18 @@ export default {
     text-align: center;
     margin-top: 30px;
 }
+
 .location-box .date {
     color: #fff;
     font-size: 20px;
     font-weight: 300;
     text-align: center;
 }
+
 .weather-box {
     text-align: center;
 }
+
 .weather-box .temp {
     display: inline-block;
     padding: 10px 25px;
@@ -151,6 +152,7 @@ export default {
     box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
     font-style: italic;
 }
+
 .weather-box .weather {
     color: #fff;
     font-size: 48px;
